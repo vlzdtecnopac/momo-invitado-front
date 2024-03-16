5e3ae6a4665fe43bfe8c5a307038e0da670736d2
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-16 17:00:30 -0500

    Update Front

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,5 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import Login from "../../components/Login/Login";
+import { useEffect } from "react";
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(localStorage.getItem("token-momo")){
+      navigate("/order-here")
+    }
+  })
   return (
     <div>
       <Login />
