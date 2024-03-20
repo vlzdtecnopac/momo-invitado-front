4d3c7c6e762b9e818f712562f28afaf8990f71b2
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-19 20:48:36 -0500

    Update Frontend

--- src/pages/loginClient/LoginClient.tsx
+++ src/pages/loginClient/LoginClient.tsx
@@ -7,6 +7,47 @@ import { useLanguage } from "../../context/Langi18nContext";
 import { Link } from "react-router-dom";
 import "./LoginClient.scss";
 
+export function EnterByEmail() {
+  const { translate } = useLanguage();
+  return (
+    <div className="email-option">
+      <p className="parrafo-subtitulo">{translate("enterEmail")}</p>
+      <div className="input">
+        <input
+          type="email"
+          placeholder={translate("email")}
+          className="email"
+        />
+      </div>
+      <button className="login-btn">{translate("signInBtn")}</button>
+    </div>
+  );
+}
+
+export function EnterByPhone() {
+  const { translate } = useLanguage();
+  return (
+    <div className="phone-option">
+      <p className="parrafo-subtitulo">{translate("enterPhone")}</p>
+      <div className="input">
+        <select className="country_id">
+          <option value="55">+55</option>
+          <option value="56">+56</option>
+          <option value="57">+57</option>
+          <option value="58">+58</option>
+          <option value="59">+59</option>
+        </select>
+        <input
+          type="number"
+          placeholder={translate("phone")}
+          className="phone"
+        />
+      </div>
+      <button className="login-btn">{translate("signInBtn")}</button>
+    </div>
+  );
+}
+
 function LoginClient() {
   const { translate } = useLanguage();
 
@@ -14,8 +55,8 @@ function LoginClient() {
     <LayoutBlank>
       <div className="login_client_page">
         <div className="left">
-        <motion.img
-              className="image"
+          <motion.img
+            className="image"
             src={imgRegister}
             alt="logo-momo"
             initial={{ opacity: 0, x: -100 }}
@@ -26,7 +67,6 @@ function LoginClient() {
               ease: [0, 0.71, 0.2, 1.01],
             }}
           />
-  
         </div>
         <div className="column-right">
           <div className="center-container">
@@ -68,48 +108,10 @@ function LoginClient() {
                   <span className="icon"></span>
                   <span className="text">{translate("email")}</span>
                 </button>
-                <hr />
               </div>
-              <div className="phone-option">
-                <p className="parrafo-subtitulo">{translate("enterPhone")}</p>
-                <div className="input">
-                  <select className="country_id">
-                    <option value="55">+55</option>
-                    <option value="56">+56</option>
-                    <option value="57">+57</option>
-                    <option value="58">+58</option>
-                    <option value="59">+59</option>
-                  </select>
-                  <input
-                    type="number"
-                    placeholder={translate("phone")}
-                    className="phone"
-                  />
-                </div>
-                <button className="login-btn">{translate("signInBtn")}</button>
-              </div>
-              <hr />
-              <div className="email-option">
-                <p className="parrafo-subtitulo">{translate("enterEmail")}</p>
-                <div className="input">
-                  <input
-                    type="email"
-                    placeholder={translate("email")}
-                    className="email"
-                  />
-                </div>
-                <button className="login-btn">{translate("signInBtn")}</button>
-              </div>
-              <Link
-                className="text-align-back"
-                to="/register"
-              >
+              <Link className="text-align-back" to="/register">
                 <div className="back">
-                  <img
-                    className="back-icon"
-                    src={back}
-                    alt="back"
-                  />
+                  <img className="back-icon" src={back} alt="back" />
                   <span className="text">{translate("backText")}</span>
                 </div>
               </Link>
