50f95e766517a86c6b581d729e1c3352753091c7
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-21 10:36:25 -0500

    Update Frontend

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,33 +1,42 @@
 import OrderResumeCard from "../OrderResumeCard/OrderResumeCard";
+import { motion } from "framer-motion";
 import "./Cart.scss";
 function Cart() {
-  return (<>
-    <button className="cart-momo"><span>Ver Carrito</span> <i className="icon-cart"></i></button>
-    <div className="cart">
-      <div className="left">
-        <div className="resume">
-          <h2 className="order-resume">Resumen de tu pedido</h2>
-          <button className="x"></button>
+  return (
+    <>
+      <button className="cart-momo">
+        <span>Ver Carrito</span> <i className="icon-cart"></i>
+      </button>
+      <motion.div
+        className="cart"
+        initial={{ x: 600 }} 
+        animate={{ x: 0 }} 
+        transition={{ duration: 1 }}
+      >
+        <div className="left">
+          <div className="resume">
+            <h2 className="order-resume">Resumen de tu pedido</h2>
+            <button className="x"></button>
+          </div>
+          <div className="product-quantity">2 productos</div>
+          <div className="container-list-product">
+            <OrderResumeCard />
+            <hr className="separator" />
+            <OrderResumeCard />
+            <hr className="separator" />
+            <OrderResumeCard />
+            <hr className="separator" />
+            <OrderResumeCard />
+          </div>
         </div>
-        <div className="product-quantity">2 productos</div>
-        <div className="container-list-product">
-        <OrderResumeCard />
-        <hr className="separator" />
-        <OrderResumeCard />
-        <hr className="separator" />
-        <OrderResumeCard />
-        <hr className="separator" />
-        <OrderResumeCard />
+        <div className="right">
+          <div className="subtotal">
+            <h3 className="subtotal-tex">Subtotal (2 productos)</h3>
+            <div className="subtotal-price">$107.00</div>
+          </div>
+          <button className="pay">Continuar al pago</button>
         </div>
-      </div>
-      <div className="right">
-        <div className="subtotal">
-          <h3 className="subtotal-tex">Subtotal (2 productos)</h3>
-          <div className="subtotal-price">$107.00</div>
-        </div>
-        <button className="pay">Continuar al pago</button>
-      </div>
-  </div>
+      </motion.div>
     </>
   );
 }
