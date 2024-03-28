6fef9eeff1a9dfcb81d48b63274eb4a6a6932642
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-28 18:02:07 -0500

    Update Frontend

---
+++ src/components/Options/OptionsExtra.tsx
@@ -0,0 +1,43 @@
+import { useLanguage } from "../../context/Langi18nContext";
+
+interface OptionsExtraProps {
+  icon: string;
+  listOptions: string[]
+}
+
+const OptionsExtra: React.FC<OptionsExtraProps> = ({ icon, listOptions }) => {
+  const { translate } = useLanguage();
+
+  return (
+    <div className="extra-list-options">
+      <div className="grid grid-noGutter-noBottom">
+        <div className="col-2">
+          <img className="icon" src={icon} alt="extra" />
+        </div>
+        <div className="col-10">
+          <ul className="list-content-options">
+            {listOptions.map((option: string )=> {
+                return (  <li>
+                    <div className="grid grid-noGutter-noBottom">
+                      <div className="col-9">
+                        <p className="text">{option}</p>
+                      </div>
+                      <div className="col-3">
+                        <span className="extra">
+                          <label>
+                            <input name="green" type="checkbox" />
+                            <span className="custom-checkbox"></span>
+                          </label>
+                        </span>
+                      </div>
+                    </div>
+                  </li>);
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OptionsExtra;
--- src/pages/merchDetail/MerchDetailPage.tsx
+++ src/pages/merchDetail/MerchDetailPage.tsx
@@ -8,6 +8,8 @@ import Slider from "../../components/Slider/Slider";
 import { useLanguage } from "../../context/Langi18nContext";
 
 import "./MerchDetailPage.scss";
+import Options from "../../components/Options/Options";
+import OptionsExtra from "../../components/Options/OptionsExtra";
 
 function MerchDetailPage() {
   const { translate } = useLanguage();
@@ -31,93 +33,15 @@ function MerchDetailPage() {
             </div>
             <div className="col-9 details-col detail-card">
               <div className="details">
-                <div className="size">
-                  <div className=" size-container container">
-                    <img
-                      className="icon"
-                      src={tshirt}
-                      alt="size"
-                    />
-                    <h3 className="text">{translate("size")}</h3>
-                  </div>
-
-                  <div className="options">
-                    <button className="option">
-                      {translate("small")} <br /> <span>S</span>
-                    </button>
-                    <button className="option">
-                      {translate("medium")} <br /> <span>M</span>
-                    </button>
-                    <button className="option">
-                      {translate("large")} <br /> <span>L</span>
-                    </button>
-                  </div>
-                </div>
-                <hr className="separator" />
-                <div className="extra">
-                  <img
-                    className="icon"
-                    src={color}
-                    alt="extra"
+              <Options
+                    distanceScrolling={320}
+                    titleOptions={translate("size")}
+                    iconOptions={tshirt}
+                    listOptions={[`${translate("small")}`, `${translate("medium")}`, `${translate("large")}`]}
+                  
                   />
-                  <div className="container">
-                    <div className="block">
-                      <h3 className="text">{translate("green")}</h3>
-                      <h3 className="text">{translate("darkBlue")}</h3>
-                      <h3 className="text">{translate("orange")}</h3>
-                      <h3 className="text">{translate("lightBlue")}</h3>
-                      <h3 className="text">{translate("cream")}</h3>
-                    </div>
-                  </div>
-
-                  <div className="extra-options">
-                    <span className="extra">
-                      <label>
-                        <input
-                          name="green"
-                          type="checkbox"
-                        />
-                        <span className="custom-checkbox"></span>
-                      </label>
-                    </span>
-                    <span className="extra">
-                      <label>
-                        <input
-                          name="darkBlue"
-                          type="checkbox"
-                        />
-                        <span className="custom-checkbox"></span>
-                      </label>
-                    </span>
-                    <span className="extra">
-                      <label>
-                        <input
-                          name="orange"
-                          type="checkbox"
-                        />
-                        <span className="custom-checkbox"></span>
-                      </label>
-                    </span>
-                    <span className="extra">
-                      <label>
-                        <input
-                          name="lightBlue"
-                          type="checkbox"
-                        />
-                        <span className="custom-checkbox"></span>
-                      </label>
-                    </span>
-                    <span className="extra">
-                      <label>
-                        <input
-                          name="cream"
-                          type="checkbox"
-                        />
-                        <span className="custom-checkbox"></span>
-                      </label>
-                    </span>
-                  </div>
-                </div>
+                <hr className="separator" />
+                <OptionsExtra listOptions={[`${translate("green")}`, `${translate("darkBlue")}`, `${translate("orange")}`, `${translate("lightBlue")}`, `${translate("cream")}`]} icon={color}/>
                 <hr className="separator" />
                 <div className="complement">
                   <h3 className="text complement_title">
