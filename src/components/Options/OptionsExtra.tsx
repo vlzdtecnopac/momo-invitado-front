1ed95714f4c3a9e7d672f9ddac988dd08aee1cf8
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-04-02 13:00:28 -0500

    Update Frontend

--- src/components/Options/OptionsExtra.tsx
+++ src/components/Options/OptionsExtra.tsx
@@ -1,25 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useProductOptionStore } from "../../store/productOption.store";
 
 interface OptionsExtraProps {
   icon: string;
   listOptions: string[];
+  attr: string;
+  multiple: boolean;
 }
 
-const OptionsExtra: React.FC<OptionsExtraProps> = ({ icon, listOptions }) => {
- 
+const OptionsExtra: React.FC<OptionsExtraProps> = ({
+  icon,
+  listOptions,
+  attr,
+  multiple = false,
+}) => {
+  const { dataProductOption } = useProductOptionStore();
+  const setDataProductOption = useProductOptionStore(
+    (state) => state.setDataProductOption
+  );
+  const [valueSelect, setValueSelect] = useState<string[]>([]);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newArray;
+    if (!multiple) {
+      setValueSelect([event.target.value]);
+    } else {
+      if (!event.target.checked) {
+        newArray = valueSelect.filter((item) => item !== event.target.value);
+      } else {
+        newArray = [...valueSelect, event.target.value];
+      }
+      setValueSelect(newArray);
+    }
+  };
+
+
+  useEffect(()=>{
+    const updatedData = {
+      ...dataProductOption,
+      [attr]: valueSelect,
+    };
+    setDataProductOption(updatedData); 
+}, [valueSelect])
+
   return (
     <div className="extra-list-options">
       <div className="grid grid-noGutter-noBottom">
         <div className="col-2">
-          <img
-            className="icon"
-            src={icon}
-            alt="extra"
-          />
+          <img className="icon" src={icon} alt="extra" />
         </div>
         <div className="col-10">
           <ul className="list-content-options">
             {listOptions.map((option: string, i) => {
+              const isChecked = valueSelect.includes(option);
               return (
                 <li key={i}>
                   <div className="grid grid-noGutter-noBottom">
@@ -30,8 +63,13 @@ const OptionsExtra: React.FC<OptionsExtraProps> = ({ icon, listOptions }) => {
                       <span className="extra">
                         <label>
                           <input
-                            name="green"
+                            checked={isChecked}
+                            value={option}
                             type="checkbox"
+                            onChange={(e) => {
+                              handleCheckboxChange(e);
+                            }}
+                            name={attr}
                           />
                           <span className="custom-checkbox"></span>
                         </label>
--- src/pages/merchDetail/MerchDetailPage.tsx
+++ src/pages/merchDetail/MerchDetailPage.tsx
@@ -54,6 +54,8 @@ function MerchDetailPage() {
                     `${translate("cream")}`,
                   ]}
                   icon={color}
+                  attr="color"
+                  multiple={false}
                 />
                 <hr className="separator" />
                 <div className="complement">
