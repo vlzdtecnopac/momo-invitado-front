ac0bff783ea37e0da70d283413a59589c09e8d9f
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-04-06 12:22:44 -0500

    Update FRONTEND

--- src/pages/productsPage/ProductsPage.tsx
+++ src/pages/productsPage/ProductsPage.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { motion } from "framer-motion";
+
 import ProductCard from "../../components/ProductCard/ProductCard";
 import CategoryNav from "../../components/CategoryNav/CategoryNav";
 import Layout from "../../includes/layout/Layout";
+import no_found from "../../assets/no-found.png"
 import "./ProductsPage.scss";
 
 import axiosInstance from "../../helpers/axios.helper";
@@ -31,18 +34,26 @@ function ProductsPage() {
             {products.map((product: any) => {
               let image;
               if (product.image == "{}") {
-                image = "http://via.placeholder.com/250x250";
+                image = no_found;
               } else {
                 image = product.image.replace(/\{"/g, "").replace(/\"}/g, "");
               }
               return (
-                <div key={product.id} className="col-3_lg-3_md-3_sm-4_xs-6">
+                <motion.div key={product.id}
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 0.8,
+                  delay: 0.5,
+                  ease: [0, 0.71, 0.2, 1.01]
+                }}
+                className="col-3_lg-3_md-3_sm-4_xs-6">
                   <ProductCard
                     img={image}
                     price="30"
                     name={product.name_product}
                   />
-                </div>
+                </motion.div>
               );
             })}
           </div>
