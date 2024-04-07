daaaf5b9a7e5d542932f2260f2139611a6935de7
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-04-07 12:33:09 -0500

    Update Front

--- src/pages/merchOrCoffee/MerchOrCoffeePage.tsx
+++ src/pages/merchOrCoffee/MerchOrCoffeePage.tsx
@@ -14,13 +14,13 @@ function HotOrColdPage() {
             <div className="center-container">
               <div className="container">
                 <div className="options">
-                  <Link to="../products/Tienda">
+                  <Link to="../products/Tienda/Merch">
                     <button className="custom-bt merch">
                       <span className="icon"></span>
                       <span className="text">Merch</span>
                     </button>
                   </Link>
-                  <Link to="../products/Tienda">
+                  <Link to="../products/Tienda/Coffe">
                     <button className="custom-bt coffee-bean">
                       <span className="icon"></span>
                       <span className="text">{translate("bulkCoffee")}</span>
