d5f82a2e55ad322e419c30e5d236b5d2fea6173b
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-04-10 00:44:22 -0500

    Update Frontend

--- src/components/OrderResumeCard/OrderResumeCard.tsx
+++ src/components/OrderResumeCard/OrderResumeCard.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./OrderResumeCard.scss";
 import { db } from "../../helpers/dexie_db.helper";
-import { useShoppingStore } from "../../store/shopping.store";
 
 interface OrderResumeCardProp {
   data: any;
