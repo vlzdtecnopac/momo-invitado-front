9d6811e48106df379fdb3437d0ac449532edcfc2
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-04-09 22:13:56 -0500

    Update MOMO Invitado

--- src/pages/checkout/CheckoutPage.tsx
+++ src/pages/checkout/CheckoutPage.tsx
@@ -21,6 +21,116 @@ function MethodsCard() {
   );
 }
 
+const TipMomoClient: React.FC<any> = ({ onChange }) => {
+  const { translate } = useLanguage();
+  const [getOtherTipSatisfaction, setOtherTipSatisfaction] =
+    useState<boolean>(false);
+
+  const onHandlerTip = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const attrValue = event.currentTarget.getAttribute("value");
+    onChange(attrValue);
+  };
+
+  return (
+    <>
+      <div className="grid-2">
+        <div className="col-5">
+          <img alt="Barista" src={barista} />
+        </div>
+        <div className="col-7 tip-text">
+          <h2>{translate("baristaTipText")}</h2>
+          <div className="tip-subtext">
+            <i className="tip-icon"></i> <span>{translate("addTip")}</span>
+          </div>
+        </div>
+      </div>
+      <div className="grid-2 grid-noGutter-noBottom tip-options">
+        <div className="col">
+          <button
+            value="0"
+            onClick={(e) => onHandlerTip(e)}
+            className="tip-button"
+          >
+            <div className="percentange">0%</div>
+            <div className="middle">{translate("noTip")}</div>
+            <div>
+              <i className="dolar-icon"></i>
+            </div>
+          </button>
+          <button
+            value="5"
+            onClick={(e) => onHandlerTip(e)}
+            className="tip-button"
+          >
+            <div className="percentange">5%</div>
+            <div className="middle">¡{translate("bonus")}!</div>
+            <div>
+              <i className="dolar-icon"></i>
+            </div>
+          </button>
+        </div>
+        <div className="col-6">
+          <button
+            value="10"
+            onClick={(e) => onHandlerTip(e)}
+            className="tip-button"
+          >
+            <div className="percentange">10%</div>
+            <div className="middle">¡{translate("excellentChoice")}!</div>
+            <div>
+              <i className="dolar-icon"></i>
+            </div>
+          </button>
+          <button
+            value="15"
+            onClick={(e) => onHandlerTip(e)}
+            className="tip-button"
+          >
+            <div className="percentange">15%</div>
+            <div className="middle">¡{translate("gesture")}!</div>
+            <div>
+              <i className="dolar-icon"></i>
+            </div>
+          </button>
+        </div>
+        {!getOtherTipSatisfaction && (
+          <div className="col-12 decide">
+            <button
+              onClick={() => setOtherTipSatisfaction(true)}
+              className="tip-button_two"
+            >
+              <div className="percentange">{translate("other")}</div>
+              <div className="middle">¡{translate("youDecide")}!</div>
+              <div>
+                <i className="dolar-icon"></i>
+              </div>
+            </button>
+          </div>
+        )}
+        {getOtherTipSatisfaction && (
+          <div className="col-12 decide">
+            <div className="grid-2 grid-noGutter-noBottom">
+              <div className="col-6">
+                <button className="tip-button">
+                  <div className="percentange">{translate("other")}</div>
+                  <div className="middle">¡{translate("youDecide")}!</div>
+                  <div>
+                    <i className="dolar-icon"></i>
+                  </div>
+                </button>
+              </div>
+              <div className="col-6 decide-tip">
+                <button className="tip-button">%</button>
+                <button className="tip-button">$</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
 function MethodPayment() {
   const { translate } = useLanguage();
 
@@ -61,8 +171,7 @@ function MethodPayment() {
 
 function CheckoutPage() {
   const productCart = useLiveQuery(() => db.product.toArray());
-  const [getOtherTipSatisfaction, setOtherTipSatisfaction] =
-    useState<boolean>(false);
+  const [tipMount, setTipMount] = useState<String>();
   const { translate } = useLanguage();
 
   return (
@@ -75,93 +184,7 @@ function CheckoutPage() {
           <div className="checkout_container grid-3 grid-equalHeight">
             <div className="col-5">
               <section className="tip">
-                <div className="grid-2">
-                  <div className="col-5">
-                    <img alt="Barista" src={barista} />
-                  </div>
-                  <div className="col-7 tip-text">
-                    <h2>{translate("baristaTipText")}</h2>
-                    <div className="tip-subtext">
-                      <i className="tip-icon"></i>{" "}
-                      <span>{translate("addTip")}</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="grid-2 grid-noGutter-noBottom tip-options">
-                  <div className="col">
-                    <button className="tip-button">
-                      <div className="percentange">0%</div>
-                      <div className="middle">{translate("noTip")}</div>
-                      <div>
-                        <i className="dolar-icon"></i>
-                      </div>
-                    </button>
-                    <button className="tip-button">
-                      <div className="percentange">5%</div>
-                      <div className="middle">¡{translate("bonus")}!</div>
-                      <div>
-                        <i className="dolar-icon"></i>
-                      </div>
-                    </button>
-                  </div>
-                  <div className="col-6">
-                    <button className="tip-button">
-                      <div className="percentange">10%</div>
-                      <div className="middle">
-                        ¡{translate("excellentChoice")}!
-                      </div>
-                      <div>
-                        <i className="dolar-icon"></i>
-                      </div>
-                    </button>
-                    <button className="tip-button">
-                      <div className="percentange">15%</div>
-                      <div className="middle">¡{translate("gesture")}!</div>
-                      <div>
-                        <i className="dolar-icon"></i>
-                      </div>
-                    </button>
-                  </div>
-                  {!getOtherTipSatisfaction && (
-                    <div className="col-12 decide">
-                      <button
-                        onClick={() => setOtherTipSatisfaction(true)}
-                        className="tip-button_two"
-                      >
-                        <div className="percentange">{translate("other")}</div>
-                        <div className="middle">¡{translate("youDecide")}!</div>
-                        <div>
-                          <i className="dolar-icon"></i>
-                        </div>
-                      </button>
-                    </div>
-                  )}
-                  {getOtherTipSatisfaction && (
-                    <div className="col-12 decide">
-                      <div className="grid-2 grid-noGutter-noBottom">
-                        <div className="col-6">
-                          <button className="tip-button">
-                            <div className="percentange">
-                              {translate("other")}
-                            </div>
-                            <div className="middle">
-                              ¡{translate("youDecide")}!
-                            </div>
-                            <div>
-                              <i className="dolar-icon"></i>
-                            </div>
-                          </button>
-                        </div>
-                        <div className="col-6 decide-tip">
-                          <button className="tip-button">%</button>
-                          <button className="tip-button">$</button>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </div>
-                <hr />
-                <MethodPayment />
+                {tipMount ? (<MethodPayment />) : (<TipMomoClient onChange={(e: string) => setTipMount(e)} />)}
               </section>
             </div>
             <div className="col-4">
