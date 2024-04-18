9581ebf15c8c8db69d864c61e860ca2fef52e41a
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-04-18 15:40:21 -0500

    Update Invitado Front

--- src/components/Options/components/Coffe.tsx
+++ src/components/Options/components/Coffe.tsx
@@ -87,7 +87,7 @@ const Coffe: React.FC<CoffeProps> = ({ optionHandler }) => {
                         defaultValue={(() => {
                           switch (key) {
                             case "Tapa":
-                              return {name: "Sin Tapa", price: 0};
+                              return {name: "Sin tapa", price: 0};
                             default:
                               return {name: "", price: 0};
                           }
